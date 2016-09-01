37f74fe974bce6bc3b52e12d8881d18641b6d044
Imeobong Isang <Imeobongisang@etete-book-kai.epb.net>
2016-09-01 16:09:04 -0400

    functional commit

--- assignment1/arrayDemo.php
+++ assignment1/arrayDemo.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <body>
-  <$php
+  <?php
   $simpleArray = array();
 
   for($i = 0; $i < $_POST["arraysize"]; $i++) {
@@ -13,15 +13,23 @@
     if($simpleArray[$i] > 0) {
       $calc4 = "postive";
     }
-    else if($simpleArray[$i] < 0) {
+     if($simpleArray[$i] < 0) {
       $calc4 = "negative";
     }
-    else {
+    if($simpleArray[$i] == 0) {
       $calc4 = "zero";
     }
     $calc5 = pi()*pow($simpleArray[$i],2);
     $calc6 = (4/3)*pi()*pow((abs($simpleArray[$i])/2),3);
-  }
-  $>
+
+    print "<p>Square root: $calc1<p>" ;
+    print "<p>Element Squared: $calc2<p>" ;
+    print "<p>Element Raised To A Random Power: $calc3<p>" ;
+    print "<p>Sign of Random Element: $calc4<p>" ;
+    print "<p>Circle Area Calculation: $calc5<p>" ;
+    print "<p>Volume Area Calculation: $calc6<p>" ;
+
+   }
+  ?>
 </body>
 </html>
