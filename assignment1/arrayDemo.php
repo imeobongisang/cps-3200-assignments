92d90f9c768c8274112f8b6b4f97ee7e0f722639
Imeobong Isang <Imeobongisang@etete-book-kai.epb.net>
2016-09-01 15:29:43 -0400

    latest commit

--- assignment1/arrayDemo.php
+++ assignment1/arrayDemo.php
@@ -5,7 +5,22 @@
   $simpleArray = array();
 
   for($i = 0; $i < $_POST["arraysize"]; $i++) {
-    $simpleArray[$i] = rand($_POST["minvalue"],$_POST["maxvalue"])
+    $simpleArray[$i] = rand($_POST["minvalue"],$_POST["maxvalue"]);
+    $calc1 = sqrt(abs($simpleArray[$i]));
+    $calc2 = pow($simpleArray[$i],2);
+    $calc3 = pow($simpleArray[$i],rand(0,5));
+    $calc4 = 0;
+    if($simpleArray[$i] > 0) {
+      $calc4 = "postive";
+    }
+    else if($simpleArray[$i] < 0) {
+      $calc4 = "negative";
+    }
+    else {
+      $calc4 = "zero";
+    }
+    $calc5 = pi()*pow($simpleArray[$i],2);
+    $calc6 = (4/3)*pi()*pow((abs($simpleArray[$i])/2),3);
   }
   $>
 </body>
