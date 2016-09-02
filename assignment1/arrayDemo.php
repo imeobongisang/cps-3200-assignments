ccfef942276963369b1f7d805677a23c3e86518d
Imeobong Isang <Imeobongisang@etete-book-kai.epb.net>
2016-09-02 13:48:36 -0400

    Friday Afternoon Commit

--- assignment1/arrayDemo.php
+++ assignment1/arrayDemo.php
@@ -7,13 +7,34 @@
   <div id="back"><a href="http://localhost:8888/assignment1/arrayDemo.html">Back to form</a></div>
 
   <?php
-  if (empty($_POST["arraysize"]) || empty($_POST["minvalue"]) || empty($_POST["maxvalue"])) {
-    print "Enter integer data into every field"; 
+  if(($_POST["arraysize"]) < 1 ) {
+    print "Please enter a valid array size.";
     return ;
   }
+  if(($_POST["arraysize"] > 200)) {
+    print "Please a smaller array size.";
+    return ;
+  }
+  if($_POST["minvalue"] >$_POST["maxvalue"]) {
+    print "Error. Minimum value must be larger than maximum value.";
+    return ;
+  }
+  if(!is_numeric($_POST["arraysize"])|| !is_numeric($_POST["minvalue"]) || !is_numeric($_POST["maxvalue"])) {
+    print "Error. Invalid form data. Please enter integer data into every field.";
+    return;
+  }
+  if(empty($_POST["arraysize"]) || empty($_POST["minvalue"]) || empty($_POST["maxvalue"])) {
+    print "Error. Invalid form data. Please enter integer data into every field.";
+    return ;
+  }
+
+
+
   $simpleArray = array();
   print "<div id=\"table\">";
   print "<table style=\"width:100;\">
+  <th>Column #</th>
+  <th>Random #</th>
   <th><math><msqrt><mi><span id=\"absvalue\" style =\"border-left: 2px solid black; border-right: 2px solid black \">x</span></mi></msqrt></math></th>
   <th>x<sup>2</sup></th>
   <th>x<sup>n</sup></th>
@@ -39,8 +60,10 @@
     $calc5 = pi()*pow($simpleArray[$i],2);
     $calc6 = (4/3)*pi()*pow((abs($simpleArray[$i])/2),3);
 
-
+    $cNum = $i+1;
     print "<tr>";
+    print "<td style=\"background-color:rgb(189, 197, 255);min-width:20px;\"> $cNum.</td>";
+    print "<td>"; print$simpleArray[$i]; print "</td>";
     print "<td>";  print number_format($calc1, 3); print "</td>";
     print "<td> $calc2</td>" ;
     print "<td> $calc3</td>" ;
@@ -51,5 +74,6 @@
    }
    print "</div>"
 ?>
+
 </body>
 </html>
