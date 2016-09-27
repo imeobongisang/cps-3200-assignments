359161874b851a817d232f32a32318645a8cd522
Imeobong Isang <littleice10@gmail.com>
2016-09-26 23:42:42 -0400

    worry about it later commit

--- assignment2/fileAnalysis.php
+++ assignment2/fileAnalysis.php
@@ -12,12 +12,36 @@
     $aliceTemp = fgets($aliceHandle);
 
     if(!feof($aliceHandle)) {
-      $aliceFullStirng.= aliceTemp;
+      $aliceFullStirng.= $aliceTemp;
     }
   }
 
   fclose($aliceHandle);
-  print("<p>$aliceFullStirng</p>");
+//  print("<p>$aliceFullStirng</p>");
+
+
+  //file handle for words text
+  $wordsHandle = fopen("words.txt", "r");
+
+  //reading in the string per line for words.txt
+
+  while(!feof($wordsHandle)) {
+    $wordsTemp = fgets($wordsHandle);
+
+    if(!feof($wordsHandle)) {
+      $wordsFullString.=$wordsTemp;
+    }
+  }
+
+  $frequencyHandle = fopen("frequency.txt",w);
+
+  $record1 = "hi";
+  $record2 = "no";
+  $record3 = "really";
+  
+  fclose($aliceHanle);
+  print("<p>$wordsFullString</p>");
+
 ?>
 </body>
 </html>
